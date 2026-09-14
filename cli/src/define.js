590f34ca8fd774ee865c02adf85cf5b32b5c379a
weixin_74906904 <weixin_74906904@noreply.gitcode.com>
2026-09-14 10:28:54 +0800

    发布1.0.0

--- cli/src/define.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.commandQueueKey = exports.buildingInfoKey = exports.rendererKey = exports.version = void 0;
-exports.version = VERSION;
-exports.rendererKey = Symbol();
-exports.buildingInfoKey = Symbol();
-exports.commandQueueKey = Symbol();
-//# sourceMappingURL=define.js.map
